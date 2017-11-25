2586270d72278106a8bf59947a8863c77c26f85b
FManga <lufeboma@gmail.com>
2017-11-25 13:13:41 -0300

    implementing SSD1306 commands

--- src/components/SCREEN.js
+++ src/components/SCREEN.js
@@ -99,9 +99,11 @@ class SCREEN {
 	this.pos = 0;
 	this.fb.data.fill(0);
 	this.colStart = 0;
-	this.colEnd = 128;
+	this.colEnd = 127;
 	this.pageStart = 0;
-	this.pageEnd = 8;
+	this.pageEnd = 7;
+	this.col = 0;
+	this.page = 0;
     }
 
     state = function( data ){
@@ -113,9 +115,9 @@ class SCREEN {
 	let pe = this.pageEnd;
 	let pd = pe - ps;
 	
-	let p = this.pos++;
-	let x = cs + p % cd;
-	let y = (ps + (p / cd)|0) * 8;
+	let x = cs + this.col;
+	let y = (ps + this.page) * 8;
+	
 	for( let i=0; i<8; ++i ){
 	    let offset = ((y+i)*128 + x) * 4;
 	    let bit = ((data >>> i) & 1) * 0xE0;
@@ -125,8 +127,13 @@ class SCREEN {
 	    this.fb.data[ offset++ ] = bit;
 	}
 
-	if( this.pos >= pd*cd )
-	    this.pos = 0;
+	this.col++;
+	if( this.col > cd ){
+	    this.col = 0;
+	    this.page++;
+	    if( this.page > pd )
+		this.page = 0;
+	}
 
 	this.dirty = true;
 		 
@@ -181,17 +188,6 @@ class SCREEN {
 	} 
     }
 
-    // Set Lower Column Start Address for
-    // Page Addressing Mode 
-    cmd0(){
-    }
-    cmd1(){
-    }
-    cmd2(){
-    }// etc
-    cmdF(){
-    }
-
 
 
     // Display Off
@@ -210,10 +206,53 @@ class SCREEN {
     }
 
     // Set Segment Re-map (A0) | (b0001)
-    cmdA0(){ this.segmentRemap = 0 };
-    cmdA1(){ this.segmentRemap = 1 };
+    cmdA0(){ this.segmentRemap = 0; }
+    cmdA1(){ this.segmentRemap = 1; }
 
     cmdA5(){  }; // multiplex something or other
+
+    cmd0(){ this.colStart = this.colStart&0xF0 | 0; }
+    cmd1(){ this.colStart = this.colStart&0xF0 | 0x1; }
+    cmd2(){ this.colStart = this.colStart&0xF0 | 0x2; }
+    cmd3(){ this.colStart = this.colStart&0xF0 | 0x3; }
+    cmd4(){ this.colStart = this.colStart&0xF0 | 0x4; }
+    cmd5(){ this.colStart = this.colStart&0xF0 | 0x5; }
+    cmd6(){ this.colStart = this.colStart&0xF0 | 0x6; }
+    cmd7(){ this.colStart = this.colStart&0xF0 | 0x7; }
+    cmd8(){ this.colStart = this.colStart&0xF0 | 0x8; }
+    cmd9(){ this.colStart = this.colStart&0xF0 | 0x9; }
+    cmdA(){ this.colStart = this.colStart&0xF0 | 0xA; }
+    cmdB(){ this.colStart = this.colStart&0xF0 | 0xB; }
+    cmdC(){ this.colStart = this.colStart&0xF0 | 0xC; }
+    cmdD(){ this.colStart = this.colStart&0xF0 | 0xD; }
+    cmdE(){ this.colStart = this.colStart&0xF0 | 0xE; }
+    cmdF(){ this.colStart = this.colStart&0xF0 | 0xF; }
+
+    cmd10(){ this.colStart =            this.colStart&0x0F; }
+    cmd11(){ this.colStart = (0x1<<4) | this.colStart&0x0F; }
+    cmd12(){ this.colStart = (0x2<<4) | this.colStart&0x0F; }
+    cmd13(){ this.colStart = (0x3<<4) | this.colStart&0x0F; }
+    cmd14(){ this.colStart = (0x4<<4) | this.colStart&0x0F; }
+    cmd15(){ this.colStart = (0x5<<4) | this.colStart&0x0F; }
+    cmd16(){ this.colStart = (0x6<<4) | this.colStart&0x0F; }
+    cmd17(){ this.colStart = (0x7<<4) | this.colStart&0x0F; }
+    cmd18(){ this.colStart = (0x8<<4) | this.colStart&0x0F; }
+    cmd19(){ this.colStart = (0x9<<4) | this.colStart&0x0F; }
+    cmd1A(){ this.colStart = (0xA<<4) | this.colStart&0x0F; }
+    cmd1B(){ this.colStart = (0xB<<4) | this.colStart&0x0F; }
+    cmd1C(){ this.colStart = (0xC<<4) | this.colStart&0x0F; }
+    cmd1D(){ this.colStart = (0xD<<4) | this.colStart&0x0F; }
+    cmd1E(){ this.colStart = (0xE<<4) | this.colStart&0x0F; }
+    cmd1F(){ this.colStart = (0xF<<4) | this.colStart&0x0F; }
+
+    cmdB0(){ this.page = 0; }
+    cmdB1(){ this.page = 1; }
+    cmdB2(){ this.page = 2; }
+    cmdB3(){ this.page = 3; }
+    cmdB4(){ this.page = 4; }
+    cmdB5(){ this.page = 5; }
+    cmdB6(){ this.page = 6; }
+    cmdB7(){ this.page = 7; }
 
     // Set COM Output Scan Direction
     cmdC8(){
@@ -256,14 +295,15 @@ class SCREEN {
   // set col address range
     cmd21( v, e ){
 	this.colStart = v;
-	this.colEnd   = e+1;
+	this.colEnd   = e;
+	this.col = 0;
     }
 
   // set page address range
     cmd22( v, e ){
 	this.pageStart = v;
-	this.pageEnd   = e+1;
-	this.pos = 0;
+	this.pageEnd   = e;
+	this.page = 0;
     }
 }
 
