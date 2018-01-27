bc03f43f3b17e987e0b462bb9c068ad5e9880446
felipemanga <lufeboma@gmail.com>
2018-01-27 00:56:00 -0300

    tweaking speed

--- src/atcore/Atcore.js
+++ src/atcore/Atcore.js
@@ -388,7 +388,7 @@ t1 === undefined ? t2 : t1
 		    this.tick += 100;
 		}
 		
-		if( this.tick >= this.endTick || this.tick - lastUpdate > 100 ){
+		if( this.tick >= this.endTick || this.tick - lastUpdate > 500 ){
 		    lastUpdate = this.tick;
 		    this.updatePeriferals();
 		    while( this.history.length > 100 ) this.history.shift();
